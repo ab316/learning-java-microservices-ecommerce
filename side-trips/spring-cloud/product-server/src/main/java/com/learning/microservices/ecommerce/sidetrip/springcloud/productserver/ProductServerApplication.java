806e51c1c76709ee86d97ea814af5b211146f180
Abdullah Baig <abdullahbaig456@gmail.com>
2020-07-05 13:36:55 +0200

    SideTrip: Spring Cloud initial project

---
+++ side-trips/spring-cloud/product-server/src/main/java/com/learning/microservices/ecommerce/sidetrip/springcloud/productserver/ProductServerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.microservices.ecommerce.sidetrip.springcloud.productserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServerApplication.class, args);
+	}
+
+}
---
+++ side-trips/spring-cloud/product-server/src/test/java/com/learning/microservices/ecommerce/sidetrip/springcloud/productserver/ProductServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservices.ecommerce.sidetrip.springcloud.productserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class ProductServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
